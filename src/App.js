6d3127b22f582d5d0e141e05a45380c40a58cf3d
HowardTang <b06902020@ntu.edu.tw>
2020-09-18 05:35:11 +0800

    Add youtube music player

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React, { useState, useEffect } from "react";
-import { makeStyles, IconButton, Slider, Typography } from "@material-ui/core";
+import {
+  makeStyles,
+  Button,
+  IconButton,
+  Slider,
+  TextField,
+  Typography,
+} from "@material-ui/core";
 
 import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import ReplayIcon from "@material-ui/icons/Replay";
 import PauseIcon from "@material-ui/icons/Pause";
 import StopIcon from "@material-ui/icons/Stop";
+import YouTube from "react-youtube";
+import getYoutubeId from "get-youtube-id";
+import getYoutubeTitle from "get-youtube-title";
 import logo from "./logo.svg";
 
 const useStyles = makeStyles(() => ({
+  text: {
+    margin: "10px",
+  },
   app: {
     display: "flex",
-    maxWidth: "500px",
+    width: "500px",
     margin: "20px auto",
     border: "5px solid #0090ff",
     borderRadius: "15px",
@@ -20,7 +34,7 @@ const useStyles = makeStyles(() => ({
     width: "80px",
     margin: "10px",
     border: "1.5px solid #0090ff",
-    borderRadius: "45px",
+    borderRadius: "40px",
     animation: `$app_logo_spin infinite 5s linear`,
   },
   pause_animation: {
@@ -36,6 +50,10 @@ const useStyles = makeStyles(() => ({
   time: {
     margin: "0 10px",
   },
+  youtube: {
+    textAlign: "center",
+    display: "none",
+  },
   "@keyframes app_logo_spin": {
     from: {
       transform: "rotate(0deg)",
@@ -48,16 +66,91 @@ const useStyles = makeStyles(() => ({
 
 export default function App() {
   const classes = useStyles();
-  const [time, setTime] = useState(0);
-  const [isPlay, setIsPlay] = useState(false);
+  const [url, setUrl] = useState("https://www.youtube.com/watch?v=tXOlgAyueoo");
+  const [player, setPlayer] = useState(null);
+  const [playInfo, setPlayInfo] = useState({
+    isPlay: false,
+    time: 0,
+    length: 0,
+    title: "天亮請睜眼 feat. 邱鋒澤",
+    url: "tXOlgAyueoo",
+  });
+
+  const opts = {
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleSetUrl = (event) => {
+    setUrl(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    setPlayInfo((state) => ({
+      ...state,
+      url: getYoutubeId(url),
+    }));
+  };
+
+  const handleSetPlayer = (event) => {
+    if (event.target.getPlayerState() === -1) {
+      setPlayer(null);
+    }
+    setPlayer(event.target);
+  };
+
+  useEffect(() => {
+    if (player !== null) {
+      getYoutubeTitle(playInfo.url, (_, title) => {
+        setPlayInfo((state) => ({
+          ...state,
+          title,
+        }));
+      });
+
+      setPlayInfo((state) => ({
+        ...state,
+        isPlay: true,
+        time: Math.round(player.getCurrentTime()),
+        length: Math.round(player.getDuration()),
+      }));
+    }
+  }, [player, playInfo.url]);
+
+  useEffect(() => {
+    if (playInfo.isPlay) {
+      const interval = setInterval(() => {
+        setPlayInfo((state) => ({
+          ...state,
+          time: Math.round(player.getCurrentTime()),
+        }));
+        if (Math.round(player.getCurrentTime()) === playInfo.length) {
+          setPlayInfo((state) => ({
+            ...state,
+            isPlay: false,
+          }));
+        }
+      }, 500);
+      return () => {
+        clearInterval(interval);
+      };
+    }
+    return () => {};
+  }, [player, playInfo.isPlay, playInfo.length]);
 
   // "_" is "event"
-  const handlesetTime = (_, newPosition) => {
-    const newTime = Math.round((newPosition * 180) / 100);
-    setTime(newTime);
-    if (newTime === 180) {
-      setIsPlay(false);
+  const handleSetTime = (_, newPosition) => {
+    if (player === null) return;
+    let newTime = Math.round((newPosition * playInfo.length) / 100);
+    if (newTime === playInfo.length) {
+      newTime -= 1;
     }
+    player.seekTo(newTime);
+    setPlayInfo((state) => ({
+      ...state,
+      time: newTime,
+    }));
   };
 
   const addZero = (num) => {
@@ -65,60 +158,74 @@ export default function App() {
   };
 
   const timeToPosition = (t) => {
-    return (t * 100) / 180;
+    return (t * 100) / playInfo.length;
   };
 
   const timeFormat = (t) => {
     return `${addZero(Math.floor(t / 60))}:${addZero(t % 60)}`;
   };
 
-  const handleSetIsPlay = () => {
-    if (time === 180) return;
-    setIsPlay((p) => !p);
+  const handlePlayPause = (type) => () => {
+    if (type === true) {
+      player.playVideo();
+    } else {
+      player.pauseVideo();
+    }
+    setPlayInfo((state) => ({
+      ...state,
+      isPlay: type,
+    }));
   };
 
   const handleStop = () => {
-    setIsPlay(false);
-    setTime(0);
+    player.pauseVideo();
+    player.seekTo(0);
+    setPlayInfo((state) => ({
+      ...state,
+      isPlay: false,
+      time: 0,
+    }));
   };
-
-  useEffect(() => {
-    if (isPlay) {
-      const interval = setInterval(() => {
-        setTime((t) => {
-          if (t < 180) {
-            if (t === 179) setIsPlay(false);
-            return t + 1;
-          }
-          return t;
-        });
-      }, 1000);
-      return () => clearInterval(interval);
-    }
-    return () => {};
-  }, [isPlay]);
 
   return (
     <>
+      <div style={{ display: "flex", width: "500px", margin: "auto" }}>
+        <TextField
+          className={classes.text}
+          fullWidth
+          onChange={handleSetUrl}
+          label="Parse a youtube url"
+          value={url}
+          variant="outlined"
+        />
+        <Button
+          onClick={handleSubmit}
+          variant="outlined"
+          style={{ margin: "10px" }}
+        >
+          Submit
+        </Button>
+      </div>
       <div className={classes.app}>
         <img
           src={logo}
           className={`${classes.app_logo} ${
-            !isPlay && classes.pause_animation
+            !playInfo.isPlay && classes.pause_animation
           }`}
           alt="logo"
         />
         <div className={classes.app_right}>
+          {playInfo.title}
           <Slider
             className={classes.slider}
-            onChange={handlesetTime}
-            value={timeToPosition(time)}
+            onChange={handleSetTime}
+            value={timeToPosition(playInfo.time)}
           />
-          {isPlay ? (
+          {playInfo.isPlay ? (
             <IconButton
               color="primary"
               component="span"
-              onClick={handleSetIsPlay}
+              onClick={handlePlayPause(false)}
             >
               <PauseIcon fontSize="large" />
             </IconButton>
@@ -126,18 +233,36 @@ export default function App() {
             <IconButton
               color="primary"
               component="span"
-              onClick={handleSetIsPlay}
+              onClick={handlePlayPause(true)}
             >
-              <PlayArrowIcon fontSize="large" />
+              {playInfo.time === playInfo.length && playInfo.length !== 0 ? (
+                <ReplayIcon fontSize="large" />
+              ) : (
+                <PlayArrowIcon fontSize="large" />
+              )}
             </IconButton>
           )}
           <IconButton color="secondary" component="span" onClick={handleStop}>
             <StopIcon fontSize="large" />
           </IconButton>
           <Typography className={classes.time} variant="button">
-            {timeFormat(time)} / 03:00
+            {Number.isNaN(playInfo.time)
+              ? "00:00 "
+              : `${timeFormat(playInfo.time)} `}
+            /
+            {Number.isNaN(playInfo.length) || player === null
+              ? " 00:00"
+              : ` ${timeFormat(Math.round(player.getDuration()))}`}
           </Typography>
         </div>
+      </div>
+      <div className={classes.youtube}>
+        <YouTube
+          videoId={playInfo.url}
+          opts={opts}
+          onStateChange={handleSetPlayer}
+          onReady={handleSetPlayer}
+        />
       </div>
     </>
   );
